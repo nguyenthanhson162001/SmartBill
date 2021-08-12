802c979e98e00909cca2f4e473d059288fd34fda
Son <nguyennthanhson162001@gmail.com>
2021-08-13 03:37:28 +0700

    text index

--- router/index.js
+++ router/index.js
@@ -4,7 +4,7 @@ const verifyMiddleware = require('..//app/middlewarse/verifyToken')
 function router(app) {
     app.use('/api/account', account)
     app.use('/api/me', verifyMiddleware, me)
-    app.get('/', (req, res) => res.status(200).send(`this is server login API, \n login: /api/account/login - method: POST - {email,password}
+    app.get('/', (req, res) => res.status(200).send(`Well come you  visit to my server !!! This is server login API, \n login: /api/account/login - method: POST - {email,password}
     \n register: /api/account/register - method: POST - {email,password,lastName,firstName}
     \n infomation: /api/me/-infomation -  method: GET - auth-token: --- `))
 }
