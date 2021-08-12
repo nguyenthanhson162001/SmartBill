a5f5d48d671eee158c7cd9160c129fe97a58f18e
Son <nguyennthanhson162001@gmail.com>
2021-08-13 03:40:15 +0700

    text index

--- router/index.js
+++ router/index.js
@@ -6,6 +6,6 @@ function router(app) {
     app.use('/api/me', verifyMiddleware, me)
     app.get('/', (req, res) => res.status(200).send(`Well come you  visit to my server !!! This is server login API, ;;; login: /api/account/login - method: POST - {email,password}
     ;;; register: /api/account/register - method: POST - {email,password,lastName,firstName}
-    ;;; infomation: /api/me/-infomation -  method: GET - auth-token: --- `))
+    ;;; infomation: /api/me/infomation -  method: GET - auth-token: --- `))
 }
 module.exports = router;
