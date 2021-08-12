803f44b2e9a66ab9c9f086f983c2dee425b040d6
Son <nguyennthanhson162001@gmail.com>
2021-08-13 03:38:57 +0700

    text index

--- router/index.js
+++ router/index.js
@@ -4,8 +4,8 @@ const verifyMiddleware = require('..//app/middlewarse/verifyToken')
 function router(app) {
     app.use('/api/account', account)
     app.use('/api/me', verifyMiddleware, me)
-    app.get('/', (req, res) => res.status(200).send(`Well come you  visit to my server !!! This is server login API, \n login: /api/account/login - method: POST - {email,password}
-    \n register: /api/account/register - method: POST - {email,password,lastName,firstName}
-    \n infomation: /api/me/-infomation -  method: GET - auth-token: --- `))
+    app.get('/', (req, res) => res.status(200).send(`Well come you  visit to my server !!! This is server login API, ;;; login: /api/account/login - method: POST - {email,password}
+    ;;; register: /api/account/register - method: POST - {email,password,lastName,firstName}
+    ;;; infomation: /api/me/-infomation -  method: GET - auth-token: --- `))
 }
 module.exports = router;
