35a1bfb13132d183c8e4b90d136e0abdd38d2063
Son <nguyennthanhson162001@gmail.com>
2021-08-13 02:45:13 +0700

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = require('./router')
 const morgan = require('morgan')
-const post = 3001
+const post = process.env.port || 3001
 
 
 // connect DB
