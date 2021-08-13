dfe1612c624bd8233e92cd42fd01e8bfab9fec63
Son <nguyennthanhson162001@gmail.com>
2021-08-13 15:54:23 +0700

    name project

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = process.env.port || 3001
 // connect DB
 require('./config/db/mongo').connect()
 
+app.use("/public", express.static('./public'));
+app.set('view engine', 'ejs');
+
 // parse application/json
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({
@@ -15,7 +18,6 @@ app.use(express.urlencoded({
 }))
 
 app.use(morgan('tiny'))
-
 router(app)
 
 app.listen(port, () => console.log(`Server started with http://localhost:${port}`))
