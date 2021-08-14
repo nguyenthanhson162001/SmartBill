3863dda604049ec0164cf860f99940d23db50a69
Son <nguyennthanhson162001@gmail.com>
2021-08-14 12:44:48 +0700

    middlewarse info

--- router/me.js
+++ router/me.js
@@ -3,7 +3,17 @@ const router = express.Router()
 const meController = require('..//app/controller/MeController')
 const postController = require('..//app/controller/PostController')
 
-router.get('/infomation', meController.infomation)
-router.get('/mypost', postController.myPosts)
+router.get('/infomation', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+}, meController.infomation)
+router.get('/mypost', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+}, postController.myPosts)
 
 module.exports = router
