f31eb5bd6529a5a2b354dbfc0cb4c8ebb8f576fc
Son <nguyennthanhson162001@gmail.com>
2021-08-14 12:47:13 +0700

    middlewarse me

--- router/index.js
+++ router/index.js
@@ -4,7 +4,12 @@ const post = require('./post')
 const verifyMiddleware = require('..//app/middlewarse/verifyToken')
 function router(app) {
     app.use('/api/account', account)
-    app.use('/api/me', verifyMiddleware, me)
+    app.use('/api/me', function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Credentials", "true");
+        next();
+    }, verifyMiddleware, me)
     app.use('/api/post', verifyMiddleware, post)
     app.get('/', (req, res) => res.status(200).send(`Well come you  visit to my server !!! This is server login API, ;;; login: /api/account/login - method: POST - {email,password}
     ;;; register: /api/account/register - method: POST - {email,password,lastName,firstName}
