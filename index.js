d95d1bcb353e6dfe249a77d87c6cee26547f2299
Son <nguyennthanhson162001@gmail.com>
2021-08-14 12:40:46 +0700

    middlewarse

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
 
 app.use(morgan('tiny'))
 
-app.use(function (req, res, next) {
+app.all('/', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", "true");
