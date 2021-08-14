7cd58f5c036777350470d62c24f57d2cbb4d60e6
Son <nguyennthanhson162001@gmail.com>
2021-08-14 12:50:21 +0700

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 const router = require('./router')
 const morgan = require('morgan')
 const port = process.env.port || 3001
@@ -7,6 +8,9 @@ const port = process.env.port || 3001
 
 // connect DB
 require('./config/db/mongo').connect()
+
+// config Enable All CORS Requests
+app.use(cors())
 
 app.use("/public", express.static('./public'));
 app.set('view engine', 'ejs');
