4449f9910c11997a96cb9d0950043bc8975276d5
Son <nguyennthanhson162001@gmail.com>
2021-08-14 12:52:40 +0700

    clear cort

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.use(express.urlencoded({
 
 app.use(morgan('tiny'))
 
-app.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+// app.all('/', function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Credentials", "true");
+//     next();
+// });
 router(app)
 
 app.listen(port, () => console.log(`Server started with http://localhost:${port}`))
